b0ca358f8886cff7363bedf44b1b5982ed856786
성훈 <ksh@seonghun-ui-Macmini.local>
2025-08-12 11:29:18 +0900

    refactor: 불필요한 코드 또는 주석 제거

--- src/main/java/store/onuljang/config/CorsConfig.java
+++ src/main/java/store/onuljang/config/CorsConfig.java
@@ -7,18 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:3000"
-                    , "https://onuljang.store"
-                    , "https://dev.onuljang.store"
-                )
-                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+            .allowedOrigins(
+                "http://localhost:3000"
+                , "https://onuljang.store"
+                , "https://dev.onuljang.store"
+            )
+            .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
