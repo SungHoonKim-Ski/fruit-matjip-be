2b98ba5d29cec7177f281aaf9013a8c237048c62
성훈 <ksh@seonghun-ui-Macmini.local>
2025-08-12 11:32:23 +0900

    feat: 어드민 세션 로그인 구현중

---
+++ src/main/java/store/onuljang/repository/AdminRepository.java
@@ -0,0 +1,14 @@
+package store.onuljang.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import store.onuljang.repository.entity.Admin;
+
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Optional<Admin> findByName(String name);
+
+    Optional<Admin> findByEmail(String email);
+}
