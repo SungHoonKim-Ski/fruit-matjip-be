6d981fa590454524daea4bbb189cf5ca9ba039bf
성훈 <ksh@seonghun-ui-Macmini.local>
2025-08-13 09:21:25 +0900

    refactor : exception, product 이름 수정

---
+++ src/main/java/store/onuljang/controller/ProductsController.java
@@ -0,0 +1,32 @@
+package store.onuljang.controller;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import store.onuljang.appservice.ProductsAppService;
+import store.onuljang.controller.request.ProductListRequest;
+import store.onuljang.controller.response.ProductListResponse;
+
+@RestController
+@RequestMapping("/api/auth/products")
+@RequiredArgsConstructor
+@Validated
+public class ProductsController {
+    ProductsAppService productsAppService;
+
+    @GetMapping
+    public ResponseEntity<ProductListResponse> getProducts(@Valid @ModelAttribute ProductListRequest request) {
+        return ResponseEntity.ok(productsAppService.getProducts(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> detail(@PathVariable @Valid @PositiveOrZero Long id) {
+        productsAppService.getDetail(id);
+        return ResponseEntity.ok().build();
+    }
+}
+
+
