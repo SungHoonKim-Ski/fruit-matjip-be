123a2124419eaa4e131ad4d69118c8f040748cee
성훈 <ksh@seonghun-ui-Macmini.local>
2025-08-17 23:17:54 +0900

    refactor : dev/prod 환경 분리, dev의 경우 유저 로그인 필요 없도록 수정

---
+++ src/main/java/store/onuljang/appservice/DevAuthAppServiceImpl.java
@@ -0,0 +1,45 @@
+package store.onuljang.appservice;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import store.onuljang.auth.JwtUtil;
+import store.onuljang.controller.request.LoginRequest;
+import store.onuljang.controller.response.LoginResponse;
+import store.onuljang.repository.entity.RefreshToken;
+import store.onuljang.repository.entity.Users;
+import store.onuljang.service.TokenService;
+import store.onuljang.service.UserService;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Profile("!PROD")
+public class DevAuthAppServiceImpl implements AuthAppService {
+    UserService userService;
+    TokenService tokenService;
+    JwtUtil jwtUtil;
+
+    public LoginResponse socialLogin(LoginRequest request) {
+        Users user = userService.findBySocialId("7777777");
+        String accessToken = tokenService.generateToken(user);
+
+        return LoginResponse
+            .builder()
+            .name(user.getName())
+            .access(accessToken)
+            .build();
+    }
+
+    @Transactional
+    public String refresh(String accessBearerToken, String refreshToken) {
+        String userUid = jwtUtil.getUidFromExpiredToken(accessBearerToken);
+        RefreshToken curToken = tokenService.validate(userUid, refreshToken);
+
+        Users user = userService.findByUId(userUid);
+        return tokenService.generateToken(user, curToken);
+    }
+}
---
+++ src/main/java/store/onuljang/appservice/ProdAuthAppServiceImpl.java
@@ -0,0 +1,60 @@
+package store.onuljang.appservice;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import store.onuljang.auth.JwtUtil;
+import store.onuljang.controller.request.LoginRequest;
+import store.onuljang.controller.response.LoginResponse;
+import store.onuljang.feign.dto.KakaoLoginResponse;
+import store.onuljang.feign.dto.KakaoMeRespone;
+import store.onuljang.repository.entity.RefreshToken;
+import store.onuljang.repository.entity.Users;
+import store.onuljang.service.KakaoService;
+import store.onuljang.service.TokenService;
+import store.onuljang.service.UserService;
+import store.onuljang.service.UserSignupService;
+
+@Service
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Profile("PROD")
+public class ProdAuthAppServiceImpl implements AuthAppService {
+    KakaoService kakaoService;
+    UserSignupService userSignupService;
+    UserService userService;
+    TokenService tokenService;
+    JwtUtil jwtUtil;
+
+    public LoginResponse socialLogin(LoginRequest request) {
+        KakaoLoginResponse token = kakaoService.kakaoLogin(request.code(), request.redirectUri());
+        KakaoMeRespone me = kakaoService.getKakaoUserInfo(token);
+
+        Users user = userSignupService.ensureUserBySocialId(me.id());
+
+        return afterSocialLogin(user);
+    }
+
+    public LoginResponse afterSocialLogin(Users user) {
+        String accessToken = tokenService.generateToken(user);
+
+        return LoginResponse
+            .builder()
+            .name(user.getName())
+            .access(accessToken)
+            .build();
+    }
+
+
+    @Transactional
+    public String refresh(String accessBearerToken, String refreshToken) {
+        String userUid = jwtUtil.getUidFromExpiredToken(accessBearerToken);
+        RefreshToken curToken = tokenService.validate(userUid, refreshToken);
+
+        Users user = userService.findByUId(userUid);
+        return tokenService.generateToken(user, curToken);
+    }
+}
