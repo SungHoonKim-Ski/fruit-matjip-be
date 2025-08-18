b3d94755bd0eedb9232667979ffb691290572328
성훈 <ksh@seonghun-ui-Macmini.local>
2025-08-18 19:32:31 +0900

    feat : 닉네임 변경 유효성 검사 추가

--- src/main/java/store/onuljang/controller/UserController.java
+++ src/main/java/store/onuljang/controller/UserController.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import store.onuljang.appservice.UserAppService;
 import store.onuljang.auth.JwtUtil;
@@ -14,18 +15,34 @@ import store.onuljang.auth.JwtUtil;
 @RestController
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Validated
 public class UserController {
     UserAppService userAppService;
     JwtUtil jwtUtil;
 
-    @PatchMapping("/auth/name/{name}")
-    public ResponseEntity<Void> modifyName(@RequestHeader("Authorization") String authorization, @PathVariable String name) {
-        userAppService.modifyName(jwtUtil.getBearerUid(authorization), name);
+    @PatchMapping("/name/{name}")
+    public ResponseEntity<Void> modifyName(
+        @RequestHeader("Authorization") String authorization,
+        @PathVariable
+        @NotBlank
+        @Size(min = 3, max = 10, message = "이름은 3~10자여야 합니다.")
+        @Pattern(regexp = "^[A-Za-z0-9\\uAC00-\\uD7A3]+$", message = "이름은 한글, 영어, 숫자만 허용됩니다.")
+        String name
+    ) {
+        String uid = jwtUtil.getBearerUid(authorization);
+
+        userAppService.modifyName(uid, name);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/auth/name/{name}")
-    public ResponseEntity<Boolean> existName(@PathVariable @Valid @NotNull @NotBlank @Size(min=3, max=10) String name) {
+    @GetMapping("/name/{name}")
+    public ResponseEntity<Boolean> existName(
+        @PathVariable
+        @NotBlank
+        @Size(min = 3, max = 10)
+        @Pattern(regexp = "^[A-Za-z0-9\\uAC00-\\uD7A3]+$")
+        String name
+    ) {
         return ResponseEntity.ok(userAppService.existName(name));
     }
 }
