3c0fb0ffdecfaf7cf77f4dc8245586f9534d7d4e
성훈 <ksh@seonghun-ui-Macmini.local>
2026-01-11 01:25:51 +0900

    test : 테스트 s3 config 관련 누락된 import 추가

--- src/main/java/store/onuljang/config/S3Config.java
+++ src/main/java/store/onuljang/config/S3Config.java
@@ -2,6 +2,7 @@ package store.onuljang.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
