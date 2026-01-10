9b59c0f801ca63420396275fd805c32411fd5f23
성훈 <ksh@seonghun-ui-Macmini.local>
2026-01-11 02:37:13 +0900

    fix : 관리자 회원가입 valid 확인 안하는 이슈 수정

--- src/main/java/store/onuljang/controller/AdminController.java
+++ src/main/java/store/onuljang/controller/AdminController.java
@@ -1,33 +1,16 @@
 package store.onuljang.controller;
 
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import store.onuljang.appservice.AdminAppService;
-import store.onuljang.controller.request.AdminLoginRequest;
 import store.onuljang.controller.request.AdminSignupRequest;
-import store.onuljang.repository.AdminRepository;
-import store.onuljang.repository.entity.Admin;
 import store.onuljang.service.dto.AdminUserDetails;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -40,7 +23,7 @@ public class AdminController {
     // login => AdminLoginFilter
 
     @PostMapping("/signup")
-    public ResponseEntity<Long> sighup(@RequestBody AdminSignupRequest request) {
+    public ResponseEntity<Long> sighup(@RequestBody @Valid AdminSignupRequest request) {
         long res = adminAppService.signUp(request);
 
         return ResponseEntity.ok(res);
