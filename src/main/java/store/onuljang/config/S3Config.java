8c2aab0baba9e1b8a34c2bf43a351a8d27cfdf93
성훈 <ksh@seonghun-ui-Macmini.local>
2026-01-11 01:22:58 +0900

    test : s3 관련 yml 수정

--- src/main/java/store/onuljang/config/S3Config.java
+++ src/main/java/store/onuljang/config/S3Config.java
@@ -11,6 +11,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
+@ConditionalOnProperty(name = "cloud.aws.s3.enabled", havingValue = "true", matchIfMissing = true)
 public class S3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
