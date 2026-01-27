fc05a7af028fb22f1292f121e40e2ac52b123c11
성훈 <ksh@seonghun-ui-Macmini.local>
2026-01-28 02:31:28 +0900

    refactor : 배달 요청시 예약번호 중복되지 않도록 수정

--- src/main/java/store/onuljang/controller/request/DeliveryReadyRequest.java
+++ src/main/java/store/onuljang/controller/request/DeliveryReadyRequest.java
@@ -1,10 +1,12 @@
 package store.onuljang.controller.request;
 
 import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.UniqueElements;
+
 import java.util.List;
 
 public record DeliveryReadyRequest(
-    @NotEmpty List<@NotNull @Positive Long> reservationIds,
+    @NotEmpty @UniqueElements List<@NotNull @Positive Long> reservationIds,
     @NotNull @Min(0) @Max(23) Integer deliveryHour,
     @NotNull @Min(0) @Max(59) Integer deliveryMinute,
     @NotBlank String phone,
