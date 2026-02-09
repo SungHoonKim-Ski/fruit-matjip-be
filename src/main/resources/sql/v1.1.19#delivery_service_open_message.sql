9ee2b4fea3df0c30463bd1fda7d8f88bdcb39f40
성훈 <ksh@seonghun-ui-Macmini.local>
2026-02-10 02:04:00 +0900

    feat : 배송비 문구 수정

--- src/main/resources/sql/v1.1.19#delivery_service_open_message.sql
+++ src/main/resources/sql/v1.1.19#delivery_service_open_message.sql
@@ -5,16 +5,22 @@ UPDATE message_template SET priority = 50 WHERE message_type = 'USER_NO_SHOW';
 INSERT INTO message_template(title, body, message_type, priority)
 VALUES ('맛집퀵 서비스 오픈 안내',
         '오래 기다리셨습니다.
-<b>맛집퀵🚚(QUICK) 서비스</b>가 드디어 오픈되었습니다!
+<b>맛집퀵🚚(QUICK) 서비스</b>가
+드디어 오픈되었습니다!
 
 예약하신 이후,
 우측 하단 <b>맛집퀵🚚</b> 메뉴에서 결제하시면
 상품을 <b>집으로 바로 배송</b>해드립니다.
-
 <small>
-배송비 안내<br/>
-· 기본 배송비 2,900원<br/>
+배송비 안내
+· 기본 배송비 2,900원
 · 100m당 50원 추가
 </small>',
         'NOTICE',
         100);
+
+INSERT INTO user_message_queue (user_uid, message_template_id, status, created_at)
+SELECT u.uid, t.id, 'PENDING', NOW()
+FROM users u
+         CROSS JOIN message_template t
+WHERE t.message_type = 'NOTICE';
