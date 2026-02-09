e828819e6c8e6c43d37a787b1b663d1d08ebdf76
성훈 <ksh@seonghun-ui-Macmini.local>
2026-02-10 02:00:34 +0900

    feat : 배달 관련 안내문구(sql) 수정

--- src/main/resources/sql/v1.1.19#delivery_service_open_message.sql
+++ src/main/resources/sql/v1.1.19#delivery_service_open_message.sql
@@ -5,14 +5,16 @@ UPDATE message_template SET priority = 50 WHERE message_type = 'USER_NO_SHOW';
 INSERT INTO message_template(title, body, message_type, priority)
 VALUES ('맛집퀵 서비스 오픈 안내',
         '오래 기다리셨습니다.
-<b>"맛집퀵(QUICK) 서비스"</b>가
-드디어 오픈되었습니다!
+<b>맛집퀵🚚(QUICK) 서비스</b>가 드디어 오픈되었습니다!
 
-하단 <b>상품배달</b> 항목을 누르셔서
-결제해주시면
-이제 <b>집으로 제품을 보내드립니다.</b>
+예약하신 이후,
+우측 하단 <b>맛집퀵🚚</b> 메뉴에서 결제하시면
+상품을 <b>집으로 바로 배송</b>해드립니다.
 
-*기본 배송비 2,900원
-+ 100m당 50원 부과',
+<small>
+배송비 안내<br/>
+· 기본 배송비 2,900원<br/>
+· 100m당 50원 추가
+</small>',
         'NOTICE',
         100);
